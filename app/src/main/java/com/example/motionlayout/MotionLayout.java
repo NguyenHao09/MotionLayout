80ae26f5570859d6a0551be399896c1c20bb2ba3
NguyenHao09 <haonn09@gmail.com>
2021-09-16 11:01:35 +0700

    MotionLayout

---
+++ app/src/main/java/com/example/motionlayout/MotionLayout.java
@@ -0,0 +1,14 @@
+package com.example.motionlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MotionLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motion_layout);
+    }
+}
